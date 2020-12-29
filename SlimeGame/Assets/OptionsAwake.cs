c8ef6239494d83108062b2b9cb8b9925654aecd0
AlvaroGA99 <55707621+AlvaroGA99@users.noreply.github.com>
2020-12-29 03:16:57 +0100

    Opciones menú persistentes

---
+++ SlimeGame/Assets/OptionsAwake.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class OptionsAwake : MonoBehaviour
+{
+
+    [SerializeField] Slider sliderVolume;
+    [SerializeField] Slider sliderSensitivity;
+
+    [SerializeField] Button español;
+    [SerializeField] Button english;
+    void Awake(){
+        sliderVolume.value = PlayerPrefs.GetFloat("volume",1);
+        sliderVolume.onValueChanged.AddListener(delegate {AudioListener.volume = sliderVolume.value;
+                                                    PlayerPrefs.SetFloat("volume", sliderVolume.value);
+                                                    Debug.Log(sliderVolume.value); });
+
+        sliderSensitivity.value = PlayerPrefs.GetFloat("sensitivity",1);
+        sliderSensitivity.onValueChanged.AddListener(delegate {
+                                                    PlayerPrefs.SetFloat("sensitivity", sliderSensitivity.value);
+                                                    Debug.Log(sliderSensitivity.value); });
+
+        español.onClick.AddListener(delegate {PlayerPrefs.SetInt("language",1);});
+        english.onClick.AddListener(delegate {PlayerPrefs.SetInt("language",0);});
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        
+    }
+}
--- SlimeGame/Assets/Scripts/MenuController.cs
+++ SlimeGame/Assets/Scripts/MenuController.cs
@@ -9,7 +9,7 @@ public class MenuController : MonoBehaviour
 
     void Start()
     {
-        
+        AudioListener.volume = PlayerPrefs.GetFloat("volume",1);
     }
 
     // Update is called once per frame
