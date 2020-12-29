de986aba049892340a87b9aede70495b8e05b98f
AlvaroGA99 <55707621+AlvaroGA99@users.noreply.github.com>
2020-12-29 03:58:36 +0100

    Opciones in-game casi completo

---
+++ SlimeGame/Assets/Scripts/ShowOptions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowOptions : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] GameObject options;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Escape)){
+            if(!options.active){
+                options.SetActive(true);   
+            }else {
+                options.SetActive(false);   
+            }
+            
+        }
+    }
+}
