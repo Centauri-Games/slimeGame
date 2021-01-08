8e63dfd3e6305f994b1acc997933720b535a3364
AlvaroGA99 <55707621+AlvaroGA99@users.noreply.github.com>
2021-01-08 20:40:49 +0100

    Estructura basica script customización

---
+++ SlimeGame/Assets/CustomizationManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomizationManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] GameObject[] slimeList;
+    [SerializeField] GameObject[] waterGunList;
+
+    [SerializeField] GameObject[] waterGrenadeList;
+
+    [SerializeField] GameObject[] plungerList;
+
+    int slimeCursor;
+    int waterGunCursor;
+    int waterGrenadeCursor;
+    int plungerCursor;
+
+
+     void slimeCursorAdd(int valueToAdd){
+         slimeCursor +=  valueToAdd;
+         if (slimeCursor < 0){
+             slimeCursor = 0;
+         } else if(slimeCursor > slimeList.Length){
+             slimeCursor = slimeList.Length;
+         }
+     }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
