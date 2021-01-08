73fb0d1a5b8d0d643b3c6e2e5e40e33321361efd
AlvaroGA99 <55707621+AlvaroGA99@users.noreply.github.com>
2021-01-08 20:47:29 +0100

    Funciones de Cambio de skin

--- SlimeGame/Assets/CustomizationManager.cs
+++ SlimeGame/Assets/CustomizationManager.cs
@@ -28,6 +28,34 @@ public class CustomizationManager : MonoBehaviour
          }
      }
 
+     void waterGunCursorAdd(int valueToAdd){
+         waterGunCursor +=  valueToAdd;
+         if (waterGunCursor < 0){
+             waterGunCursor = 0;
+         } else if(waterGunCursor > waterGunList.Length){
+             waterGunCursor = waterGunList.Length;
+         }
+     }
+
+      void waterGrenadeCursorAdd(int valueToAdd){
+         waterGrenadeCursor +=  valueToAdd;
+         if (waterGrenadeCursor < 0){
+             waterGrenadeCursor = 0;
+         } else if(waterGrenadeCursor > waterGrenadeList.Length){
+             waterGrenadeCursor = waterGrenadeList.Length;
+         }
+     }
+
+      void plungerCursorAdd(int valueToAdd){
+         plungerCursor +=  valueToAdd;
+         if (plungerCursor < 0){
+             plungerCursor = 0;
+         } else if(plungerCursor > plungerList.Length){
+             plungerCursor = plungerList.Length;
+         }
+     }
+
+
     void Start()
     {
         
