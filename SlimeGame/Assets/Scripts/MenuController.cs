07b831cc15c51024036086fce604304c0b83f7fe
AlvaroGA99 <55707621+AlvaroGA99@users.noreply.github.com>
2021-01-11 00:24:38 +0100

    Interfaz nickanem hecha
    Falta funcionalidad

--- SlimeGame/Assets/Scripts/MenuController.cs
+++ SlimeGame/Assets/Scripts/MenuController.cs
@@ -3,11 +3,39 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.UI;
 
-public class MenuController : MonoBehaviour
+public class MenuController : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
+    public GameObject connecting;
+    public GameObject nickname;
 
+    public GameObject button1;
+    public GameObject button2;
+    public GameObject button3;
+    public GameObject button4;
+
+    public void Connecttoserver()
+    {
+        nickname.SetActive(false);
+
+        if (!PhotonNetwork.IsConnected)
+        {
+            if (PhotonNetwork.ConnectUsingSettings())
+            {
+                connecting.SetActive(true);
+            }
+            else
+            {
+                SceneManager.LoadScene("MainMenu");
+            }
+        }
+    }
+    public override void OnConnectedToMaster(){
+        SceneManager.LoadScene("GameMenu");
+        
+    }
     void Start()
     {
         AudioListener.volume = PlayerPrefs.GetFloat("volume",1);
@@ -19,6 +47,14 @@ public class MenuController : MonoBehaviour
         
     }
 
+
+    public void showNickname(){
+        button1.SetActive(false);
+        button2.SetActive(false);
+        button3.SetActive(false);
+        button4.SetActive(false);
+         nickname.SetActive(true);
+    }
     public void goToMatchmakingScene(){
         SceneManager.LoadScene("GameMenu");
     }
@@ -36,4 +72,7 @@ public class MenuController : MonoBehaviour
     public void goToShopScene(){
         SceneManager.LoadScene("ShopScene");
     }
+
+    
+    
 }
