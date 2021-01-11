bd2d4a0621bcfbe8dcb0b3a4c93dd70389230d7c
deividmcmj <48552318+deividmcmj@users.noreply.github.com>
2021-01-11 17:40:26 +0100

    Algunas animaciones metidas en otro script

---
+++ SlimeGame/Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    [SerializeField] Animator slimeAnimatorController;
+
+    CharacterController characterController;
+
+    public void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slimeAnimatorController.SetBool("Moving", false);
+
+        if (Input.GetButton("Run"))
+        {
+            slimeAnimatorController.SetBool("Moving", true);
+        }
+
+        if (characterController.isGrounded)
+        {
+            slimeAnimatorController.SetBool("Grounded", true);
+
+            if (Input.GetButtonDown("Jump"))    //Salto si suelo o pared
+            {
+                slimeAnimatorController.SetBool("Grounded", false);
+            }
+        }
+
+
+    }
+}
