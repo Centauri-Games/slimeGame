c7fe52b0159bad5f7ccc9815c40cd47ef9589829
deividmcmj <48552318+deividmcmj@users.noreply.github.com>
2021-01-12 16:33:09 +0100

    Primera funcionalidad tienda

---
+++ SlimeGame/Assets/Scripts/ShopScene.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ShopScene : MonoBehaviour
+{
+    public GameObject button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerPrefs.GetInt("language") == 1)
+        {
+            button.GetComponentInChildren<Text>().text = "Atrás";
+        }
+        else
+        {
+            button.GetComponentInChildren<Text>().text = "Back";
+        }
+    }
+
+
+    public void goBackToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
